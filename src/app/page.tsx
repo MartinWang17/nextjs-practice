de1cbf6c35e5ab457e6dacae7b44f84432cbe823
Martin Wang <martinwangcs.dev@gmail.com>
2025-06-04 21:49:06 -0400

    Add basic styling to the home page with a dedicated css file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { getPosts } from './postsData'
+import styles from './home.module.css'
 
 const posts = getPosts();
 
 export default function HomePage() {
   return (
-    <main>
-      <h1>My Blog</h1>
-      <ul> 
+    <main className={styles.main}>
+      <h1 className={styles.title}>My Blog</h1>
+      <ul className={styles.postsList}> 
         {posts.map(post => (
           <li key={post.slug}>
             <a href={`/posts/${post.slug}`}>{post.title}</a>
